a2065d5844745e9ae75962965140dffb763f748c
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-01 07:19:39 -0500

    created project

---
+++ CoreCerraduraClient/CoreCerraduraClient/CoreCerraduraClient.h
@@ -0,0 +1,19 @@
+//
+//  CoreCerraduraClient.h
+//  CoreCerraduraClient
+//
+//  Created by Alsey Coleman Miller on 5/29/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreCerraduraClient.
+FOUNDATION_EXPORT double CoreCerraduraClientVersionNumber;
+
+//! Project version string for CoreCerraduraClient.
+FOUNDATION_EXPORT const unsigned char CoreCerraduraClientVersionString[];
+
+
+
+
